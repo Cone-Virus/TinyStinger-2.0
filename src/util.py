736fe5c89a1c45d42a41196806ac3b2192b44b76
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-07-21 21:15:29 -0500

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,12 +1,27 @@
 import os
 from colorama import Fore, Back, Style
+import magic
 
-def file_check(given_file):
-    if os.path.isfile(given_file):
-        print(Fore.GREEN + Style.BRIGHT + "Target file Validated")
+def file_check(indicator,given_file):
+    if os.path.isfile(given_file) and magic.from_file(given_file) == 'ASCII text':
+        print(Fore.GREEN + Style.BRIGHT + "[+] " + indicator + " file Validated")
     else:
-        print(Fore.RED + Style.BRIGHT + "Target file could not be Validated")
+        print(Fore.RED + Style.BRIGHT + "[-] " + indicator + " file could not be Validated")
         quit()
+
+def file_read(indicator,given_file):
+    file_check(indicator,given_file)
+    the_list =  open(given_file,'r').readlines()
+    # Generate List
+    variable_list = []
+    for variable in the_list:
+        variable = variable.strip('\n')
+        # Ignore empty lines
+        if variable != "":
+            variable_list.append(variable)
+    print(Fore.GREEN + Style.BRIGHT + "[+] All " + indicator + "s Processed")
+    return variable_list
+
 
 def banner():
     banner = '''▀█▀ █ █▄░█ █▄█   █▀ ▀█▀ █ █▄░█ █▀▀ █▀▀ █▀█
