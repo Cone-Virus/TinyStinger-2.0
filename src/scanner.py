fde9bea57f695fccf2a3a06d95a8fd5c19c48ed0
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-07-21 21:15:09 -0500

    Update scanner.py

--- src/scanner.py
+++ src/scanner.py
@@ -8,8 +8,28 @@ def waf_scan(target):
     for row in waf_results:
         if '[+]' in row:
             if 'WAF' in row:
-                print(Fore.RED + Style.BRIGHT + "[-] WAF Found!")
+                print(Fore.RED + Style.BRIGHT + "[-] WAF Found")
                 waf_type = re.search("behind (.*) WAF",row)
                 return waf_type.group(1)
-    print(Fore.GREEN + Style.BRIGHT + "[+] No WAF Found!")
+    print(Fore.GREEN + Style.BRIGHT + "[+] No WAF Found")
     return "N/A"
+
+def remove_exclusions(exclusions,targets):
+    exclusion_bool = False
+    num = 0
+    temp_targets = []
+    for target in targets:
+        for ex in exclusions:
+            if re.match(".*" + ex + ".*",target):
+                exclusion_bool = True
+            else:
+                print(Fore.MAGENTA + Style.BRIGHT + "[+] Removing Exclusions" + num * ".", end='\r')
+                num = num + 1
+                if num == 4:
+                    num = 0
+        if exclusion_bool:
+            exclusion_bool = False
+        else:
+            temp_targets.append(target)
+    print()
+    return temp_targets
