f76133c9bc09e70b5b054b7aad6ed4629ee290bb
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-07-23 18:55:41 -0500

    Update TinyStinger.py

--- TinyStinger.py
+++ TinyStinger.py
@@ -13,25 +13,26 @@ import util as util
 util.banner()
 
 # Argument Evaluation
-parser = argparse.ArgumentParser()
-parser.add_argument("db_name", metavar="<Database Name>", help="The name you would like to give the database")
-parser.add_argument("target_list", metavar="<Target List>", help="List of Targets")
-parser.add_argument("--exclude",  help="List of Out of Scope Targets")
-
-args = parser.parse_args()
-db_name = args.db_name
-target_file = args.target_list
-exclude_file = args.exclude
+args = util.arg_setup()
+if args.which == "scan":
+    db_name = args.db_name
+    target_file = args.target_list
+    exclude_file = args.exclude
+elif args.which == "shell":
+    db = store.load_db(args.db_name)
+    store.shell_db(db)
 
 # Create DB
 db = store.create_db(db_name)
 
 # Scan targets in file
-target_list = util.file_read("Target",target_file)
+target_list = util.target_format(scan.http_valid(util.file_read("Target",target_file),args.nohttp))
 
+# If blacklist given
 if exclude_file != None:
     target_list = scan.remove_exclusions(util.file_read("Exclusion",exclude_file),target_list)
 
+# Scan
 for target in target_list:
     # Reset Colors
     print(Style.RESET_ALL)
@@ -39,8 +40,12 @@ for target in target_list:
     print(Fore.BLUE + Style.BRIGHT + "[+] " + target)
     # WAF Scan
     waf_result = scan.waf_scan(target)
-    value = [target,waf_result]
+    # Fav Scan
+    fav_result = scan.fav_scan(target)
+    value = [target,waf_result,fav_result]
     store.insert_db(db,value)
 
-# Close DB
-store.close_db(db)
+# Start shell if option noshell is not set
+if args.noshell:
+    store.shell_db(db)
+
