31007d0f6550d59e11b23915d673e49a2c426b3d
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-07-23 18:56:06 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ sudo pip3 install -r requirements.txt
 mkdir src/wafw00f
 git clone https://github.com/EnableSecurity/wafw00f src/wafw00f
 sudo src/wafw00f/setup.py install
-
+git clone https://github.com/devanshbatham/FavFreak src/favfreak
+sudo pip3 install -r src/favfreak/requirements.txt
