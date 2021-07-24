ad43cd9acbfe3bbccc4f1eb048d15fd01fde57a6
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-07-24 13:42:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ git clone https://github.com/EnableSecurity/wafw00f src/wafw00f
 sudo src/wafw00f/setup.py install
 git clone https://github.com/devanshbatham/FavFreak src/favfreak
 sudo pip3 install -r src/favfreak/requirements.txt
+wget https://github.com/projectdiscovery/subfinder/releases/download/v2.4.8/subfinder_2.4.8_linux_amd64.tar.gz
+mkdir src/subfinder
+mv subfinder_2.4.8_linux_amd64.tar.gz src/subfinder
+cd src/subfinder
+tar xvzf subfinder_2.4.8_linux_amd64.tar.gz
+cd ../..
