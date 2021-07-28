6e0d01a2dfad81feefd460d063fd870efa143576
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-07-28 11:07:01 -0500

    Update db_maker.py

--- src/db_maker.py
+++ src/db_maker.py
@@ -1,8 +1,7 @@
 import sqlite3
-from colorama import Fore, Back, Style
 import os
 import magic
-import commands as com
+import command as com
 import re
 
 def target_db(target,db_name):
@@ -33,98 +32,23 @@ def close_db(db):
 
 def create_db(name):
     db_name = "storage/" + name + "/" +name + ".db"
-    try:
-        os.mkdir("storage/" + name, 0o755 )
-        db = sqlite3.connect(db_name)
-        format_db(db)
-        print(Fore.GREEN + Style.BRIGHT + "[+] DB Created Sucessfully")
-        return db
-    except:
-        print(Fore.RED + Style.BRIGHT + "[-] DB Could not be Created")
-        quit()
-
-def load_db(db_name):
-    if os.path.exists(db_name):
-        if "SQLite 3.x" in magic.from_file(db_name):
-            try:
-                db = sqlite3.connect(db_name)
-                print(Fore.GREEN + Style.BRIGHT + "[+] DB Successfully Loaded")
-                return db
-            except:
-                print(Fore.RED + Style.BRIGHT + "[-] DB Failed to Load")
-        else:
-            print(Fore.RED + Style.BRIGHT + "[-] DB is not correct format")
-            quit()
-    else:
-        print(Fore.RED + Style.BRIGHT + "[-] DB Does not exist")
-        quit()
-
-def help_menu():
-    menu = """
-▀█▀ █ █▄░█ █▄█   █▀ █░█ █▀▀ █░░ █░░
-░█░ █ █░▀█ ░█░   ▄█ █▀█ ██▄ █▄▄ █▄▄
+    os.mkdir("storage/" + name, 0o755 )
+    db = sqlite3.connect(db_name)
+    format_db(db)
+    return db
     
-Options:
 
-help : Show this menu again
-quit : Leave this shell
-
-all : Show all Target Urls
-
-mass <check> [yes/no] : Mass search through data
-mass Commands:
-check = fav,waf
-[yes/no] = If data is or isn't available
-
-select <Target Url> : Select which URL Look through
-select Commands:
-show <data> : Show data
-    show Commands:
-    data = stats,spider
-        data Commands:
-        stats = fav,waf (AKA the little stuff)
-        spider = Show spider results
-
-delete <Target url> : Delete URL from DB
-    """
-    print(Fore.YELLOW + Style.BRIGHT + menu)
-
-def shell_db(db):
-    cur = db.cursor()
-    print(Fore.GREEN + Style.DIM + "\n[%] Starting Shell")
-    help_menu()
-    url = "N/A"
-    while True:
-        command = input(Fore.YELLOW + Style.BRIGHT + "[Target: " + url + "]" + "\n#> " + Fore.GREEN)
-        if command == "quit":
-            print(Fore.GREEN + Style.BRIGHT + "\n[X] Closing Shell")
-            close_db(db)
-        elif command == "all":
-            com.all(cur)
-        elif command == "help":
-            help_menu()
-        elif re.match("^delete .*",command):
-            command = command.split(" ")
-            delete_url = com.check_url(command[1],cur)
-            if delete_url == "N/A":
-                print(Fore.RED + Style.BRIGHT + "[-] Invalid URL\n")
+def load_db(db_name,switch):
+    if switch == 0:
+        if os.path.exists(db_name):
+            if "SQLite 3.x" in magic.from_file(db_name):
+                return True
             else:
-                com.delete(delete_url,cur)
-                db.commit()
-        elif re.match("^select .*",command):
-            command = command.split(" ")
-            url = com.check_url(command[1],cur)
-        elif re.match("^mass .* .*",command):
-            command = command.split(" ")
-            com.mass(command[1],command[2],cur)
-        elif re.match("^show .*",command):
-            if url == "N/A":
-                print(Fore.RED + Style.BRIGHT + "\n[-] No URL selected\n")
-            else:
-                command = command.split(" ")
-                com.show(url,command[1],cur)
+                return False
         else:
-            print(Fore.RED + Style.BRIGHT + "[-] Unrecognized Command")
-            help_menu()
+            return False
+    elif switch == 1:
+        db = sqlite3.connect(db_name)
+        return db
 
 
